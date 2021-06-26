80adcf649d81ea43fdb9afe64bc031c3e2728d3f
Thebigboss33 <Thebigboss33@users.noreply.github.com>
2021-06-26 14:35:59 +0200

    Init

---
+++ assets/js/api.js
@@ -0,0 +1,43 @@
+function login (username, password){
+
+    var data = 'data= {'+
+    '"identifiant": "' + username + '" , "motdepasse": "' + password + '"}';
+    
+    axios.post(
+        "https://api.ecoledirecte.com/v3/login.awp",
+        data
+    ).then((res) => {
+        
+        if (res.data.code == 200) {
+            window.sessionStorage.clear();
+            window.sessionStorage.setItem('token', res.data.token);
+            window.sessionStorage.setItem('data', JSON.stringify(res.data.data.accounts[0]));
+            window.location = "http://127.0.0.1:5500/dashboard.html"
+        }else{
+            document.getElementById('error').innerHTML = "Erreur : " + JSON.stringify(res.data.message).slice(1,-1)
+        }
+
+    }).catch((e) => {
+        console.log(e)
+    });
+}
+
+function getTimeLine(id, token) {
+
+    var data = 'data= {'+
+    '"token": "' + token + '"}';
+    
+    axios.post(
+        "https://api.ecoledirecte.com/v3/eleves/" + id + "/timeline.awp?verbe=get",
+        data
+    ).then((res) => {
+        
+        if (res.data.code == 200) {
+            console.log(res.data); 
+        }
+
+    }).catch((e) => {
+        console.log(e)
+    });
+
+}
---
+++ assets/js/login.js
@@ -0,0 +1,50 @@
+function sendLogin() {
+
+    const username = document.getElementById('identifiant').value;
+    const password = document.getElementById('pass').value;
+
+    if(!username){
+        document.getElementById('error').innerHTML = "Erreur : Un identifiant doit etre mis"
+        return
+    }
+
+    if(!password){
+        document.getElementById('error').innerHTML = "Erreur : Un mot de passe doit etre mis"
+        return
+    }
+
+    login(username, password);
+
+}
+
+function checkSession(){
+
+    if(window.sessionStorage.getItem('token') && window.sessionStorage.getItem('data')){
+
+        const id = JSON.parse(window.sessionStorage.getItem('data')).id;
+        const token = window.sessionStorage.getItem('token')
+
+        var data = 'data= {'+
+        '"token": "' + token + '"}';
+        
+        axios.post(
+            "https://api.ecoledirecte.com/v3/eleves/" + id + "/timeline.awp?verbe=get",
+            data
+        ).then((res) => {
+            
+            if (res.data.code == 200) {
+                window.location = "http://127.0.0.1:5500/dashboard.html"
+            }else{
+                window.sessionStorage.clear()
+                window.location = "http://127.0.0.1:5500/login.html"
+            }
+
+        }).catch((e) => {
+            console.log(e)
+        });
+
+    }else{
+        window.sessionStorage.clear()
+    }
+
+}
