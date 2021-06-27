7ef4b5bc6231aa43729694cdd7e461b64f1efe2f
Thebigboss33 <Thebigboss33@users.noreply.github.com>
2021-06-27 11:55:07 +0200

    Update for family account

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -2,40 +2,71 @@ function checkSession(){
 
     if(window.sessionStorage.getItem('token') && window.sessionStorage.getItem('data')){
 
-        const id = JSON.parse(window.sessionStorage.getItem('data')).id;
-        const token = window.sessionStorage.getItem('token')
+        if(JSON.parse(window.sessionStorage.getItem('data')).typeCompte == "E"){
 
-        var data = 'data= {'+
-        '"token": "' + token + '"}';
-        
-        axios.post(
-            "http://api.ecoledirecte.com/v3/eleves/" + id + "/timeline.awp?verbe=get",
-            data
-        ).then((res) => {
+            const id = JSON.parse(window.sessionStorage.getItem('data')).id;
+            const token = window.sessionStorage.getItem('token')
+
+            var data = 'data= {'+
+            '"token": "' + token + '"}';
             
-            if (res.data.code == 200) {
+            axios.post(
+                "http://api.ecoledirecte.com/v3/eleves/" + id + "/timeline.awp?verbe=get",
+                data
+            ).then((res) => {
+                
+                if (res.data.code == 200) {
+                
+                    const account = JSON.parse(window.sessionStorage.getItem('data'));
+
+                    const photoUrl = "https://" + account.profile.photo.substring(2);
+
+                    document.getElementById('title').innerHTML = document.getElementById('title').innerHTML + account.prenom.charAt(0) + account.prenom.substring(1).toLowerCase() + " " + account.nom.charAt(0) + account.nom.substring(1).toLowerCase()
+                    document.getElementById('pdp').src = photoUrl;
+
+
+                }else{
+                    window.sessionStorage.clear()
+                    window.location = "http://better-ecole-directe.ml/login.html"
+                }
+
+            }).catch((e) => {
+                console.log(e)
+            });
+
+        }else if(JSON.parse(window.sessionStorage.getItem('data')).typeCompte == "1"){
+
+            const id = JSON.parse(window.sessionStorage.getItem('data')).id;
+            const token = window.sessionStorage.getItem('token')
+
+            var data = 'data= {'+
+            '"token": "' + token + '"}';
             
-                const account = JSON.parse(window.sessionStorage.getItem('data'));
-                const photoUrl = "https://" + account.profile.photo.substring(2);
-                const photoUrl2 = account.profile.photo
+            axios.post(
+                "https://api.ecoledirecte.com/v3/1/" + id + "/timelineAccueilCommun.awp?verbe=get",
+                data
+            ).then((res) => {
+                
+                if (res.data.code == 200) {
+                
+                    const account = JSON.parse(window.sessionStorage.getItem('data'));
 
-                document.getElementById('title').innerHTML = document.getElementById('title').innerHTML + account.prenom.charAt(0) + account.prenom.substring(1).toLowerCase() + " " + account.nom.charAt(0) + account.nom.substring(1).toLowerCase()
-                document.getElementById('pdp').src = photoUrl;
+                    document.getElementById('title').innerHTML = document.getElementById('title').innerHTML + account.prenom.charAt(0) + account.prenom.substring(1).toLowerCase() + " " + account.nom.charAt(0) + account.nom.substring(1).toLowerCase()
 
-                axios.get(photoUrl, {
-                    headers: {
-                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36'
-                    }
-                });
 
-            }else{
-                window.sessionStorage.clear()
-                window.location = "http://better-ecole-directe.ml/login.html"
-            }
+                }else{
+                    window.sessionStorage.clear()
+                    window.location = "http://better-ecole-directe.ml/login.html"
+                }
 
-        }).catch((e) => {
-            console.log(e)
-        });
+            }).catch((e) => {
+                console.log(e)
+            });
+
+        }else{
+            window.sessionStorage.clear()
+            window.location = "http://better-ecole-directe.ml/login.html"
+        }
 
     }else{
         window.sessionStorage.clear()
--- assets/js/login.js
+++ assets/js/login.js
@@ -21,30 +21,59 @@ function checkSession(){
 
     if(window.sessionStorage.getItem('token') && window.sessionStorage.getItem('data')){
 
-        const id = JSON.parse(window.sessionStorage.getItem('data')).id;
-        const token = window.sessionStorage.getItem('token')
+        if(JSON.parse(window.sessionStorage.getItem('data')).typeCompte == "E"){
 
-        var data = 'data= {'+
-        '"token": "' + token + '"}';
-        
-        axios.post(
-            "https://api.ecoledirecte.com/v3/eleves/" + id + "/timeline.awp?verbe=get",
-            data
-        ).then((res) => {
+            const id = JSON.parse(window.sessionStorage.getItem('data')).id;
+            const token = window.sessionStorage.getItem('token')
+
+            var data = 'data= {'+
+            '"token": "' + token + '"}';
             
-            if (res.data.code == 200) {
-                window.location = "http://better-ecole-directe.ml/dashboard.html"
-            }else{
-                window.sessionStorage.clear()
-                window.location = "http://better-ecole-directe.ml/login.html"
-            }
+            axios.post(
+                "http://api.ecoledirecte.com/v3/eleves/" + id + "/timeline.awp?verbe=get",
+                data
+            ).then((res) => {
+                
+                if (res.data.code == 200) {
+                    window.location = "http://better-ecole-directe.ml/dashboard.html"
+                }else{
+                    window.sessionStorage.clear()
+                    window.location.reload()
+                }
 
-        }).catch((e) => {
-            console.log(e)
-        });
+            }).catch((e) => {
+                console.log(e)
+            });
 
-    }else{
-        window.sessionStorage.clear()
+        }else if(JSON.parse(window.sessionStorage.getItem('data')).typeCompte == "1"){
+
+            const id = JSON.parse(window.sessionStorage.getItem('data')).id;
+            const token = window.sessionStorage.getItem('token')
+
+            var data = 'data= {'+
+            '"token": "' + token + '"}';
+            
+            axios.post(
+                "https://api.ecoledirecte.com/v3/1/" + id + "/timelineAccueilCommun.awp?verbe=get",
+                data
+            ).then((res) => {
+                
+                if (res.data.code == 200) {
+                    window.location = "http://better-ecole-directe.ml/dashboard.html"
+                }else{
+                    window.sessionStorage.clear()
+                    window.location.reload()
+                }
+
+            }).catch((e) => {
+                console.log(e)
+            });
+
+        }else{
+            window.sessionStorage.clear()
+            window.location.reload()
+        }
+
     }
 
 }
